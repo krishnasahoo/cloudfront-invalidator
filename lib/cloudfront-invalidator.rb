96bb39884b75c54f673642656c937d159ab4ac89
Krishna <krishnaa.sahoo@gmail.com>
2013-07-30 15:05:19 +0530

    fixed doc.elements["Invalidation/Status"] error

--- lib/cloudfront-invalidator.rb
+++ lib/cloudfront-invalidator.rb
@@ -4,8 +4,8 @@ require 'rexml/document'
 require 'hmac-sha1' # this is a gem
 
 class CloudfrontInvalidator  
-  BACKOFF_LIMIT = 8192
-  BACKOFF_DELAY = 0.025
+ BACKOFF_LIMIT = 8192
+ BACKOFF_DELAY = 0.025
 
   def initialize(aws_key, aws_secret, cf_dist_id, options={})
     @aws_key, @aws_secret, @cf_dist_id = aws_key, aws_secret, cf_dist_id
@@ -22,8 +22,8 @@ class CloudfrontInvalidator
   end
 
   def invalidate(*keys)
-    keys = keys.flatten.map do |k| 
-      k.start_with?('/') ? k : '/' + k 
+    keys = keys.flatten.map do |k|
+      k.start_with?('/') ? k : '/' + k
     end
     
     uri = URI.parse "#{base_url}#{@cf_dist_id}/invalidation"
@@ -39,22 +39,22 @@ class CloudfrontInvalidator
    
       # Create and raise an exception for any error the API returns to us.
       if resp.code.to_i != 201
-        error_code = doc.elements["ErrorResponse/Error/Code"].text
+        error_code = doc.elements["ErrorResponse/Error/Code"][0].to_s
         self.class.const_set(error_code,Class.new(StandardError)) unless self.class.const_defined?(error_code.to_sym)
-        raise self.class.const_get(error_code).new(doc.elements["ErrorResponse/Error/Message"].text)
+        raise self.class.const_get(error_code).new(doc.elements["ErrorResponse/Error/Message"])
       end
     
     # Handle the common case of too many in progress by waiting until the others finish.
     rescue TooManyInvalidationsInProgress => e
       sleep delay * BACKOFF_DELAY
-      delay *= 2 unless delay >= BACKOFF_LIMIT 
+      delay *= 2 unless delay >= BACKOFF_LIMIT
       STDERR.puts e.inspect
       retry
     end
 
     # If we are passed a block, poll on the status of this invalidation with truncated exponential backoff.
     if block_given?
-      invalidation_id = doc.elements["Invalidation/Id"].text
+      invalidation_id = doc.elements["Invalidation/Id"][0]
       poll_invalidation(invalidation_id) do |status,time|
         yield status, time
       end
@@ -67,7 +67,7 @@ class CloudfrontInvalidator
     delay = 1
     loop do
       doc = REXML::Document.new get_invalidation_detail_xml(invalidation_id)
-      status = doc.elements["Invalidation/Status"].text
+      status = doc.elements["Invalidation/Status"][0]
       yield status, Time.now - start
       break if status != "InProgress"
       sleep delay * BACKOFF_DELAY
@@ -83,11 +83,11 @@ class CloudfrontInvalidator
     resp = http.send_request 'GET', uri.path, '', headers
 
     doc = REXML::Document.new resp.body
-    puts "MaxItems " + doc.elements["InvalidationList/MaxItems"].text + "; " + (doc.elements["InvalidationList/MaxItems"].text == "true" ? "truncated" : "not truncated")
+    puts "MaxItems " + doc.elements["InvalidationList/MaxItems"][0] + "; " + (doc.elements["InvalidationList/MaxItems"][0] == "true" ? "truncated" : "not truncated")
 
     doc.each_element("/InvalidationList/InvalidationSummary") do |summary|
-      invalidation_id = summary.elements["Id"].text
-      summary_text = "ID " + invalidation_id + ": " + summary.elements["Status"].text
+      invalidation_id = summary.elements["Id"]
+      summary_text = "ID " + invalidation_id + ": " + summary.elements["Status"]
 
       if show_detail
         detail_doc = REXML::Document.new get_invalidation_detail_xml(invalidation_id)
@@ -97,9 +97,9 @@ class CloudfrontInvalidator
              '; Caller reference: "' +
              detail_doc.elements["Invalidation/InvalidationBatch/CallerReference"].text +
              '"'
-        puts '  Invalidated URL paths:'
+        puts ' Invalidated URL paths:'
         
-        puts "    " + detail_doc.elements.to_a('Invalidation/InvalidationBatch/Path').map { |path| path.text }.join(" ")
+        puts " " + detail_doc.elements.to_a('Invalidation/InvalidationBatch/Path').map { |path| path.text }.join(" ")
       else
         puts summary_text
       end
@@ -123,13 +123,13 @@ class CloudfrontInvalidator
     xml = <<XML
 <?xml version="1.0" encoding="UTF-8"?>
 <InvalidationBatch xmlns="#{doc_url}">
-  <Paths>
-    <Quantity>#{keys.size}</Quantity>
-    <Items>
-      #{keys.map{|k| "<Path>#{k}</Path>" }.join("\n    ")}
-    </Items>
-  </Paths>
-  <CallerReference>#{self.class.to_s} on #{Socket.gethostname} at #{Time.now.to_i}</CallerReference>"
+<Paths>
+<Quantity>#{keys.size}</Quantity>
+<Items>
+#{keys.map{|k| "<Path>#{k}</Path>" }.join("\n ")}
+</Items>
+</Paths>
+<CallerReference>CloudfrontInvalidator on #{Socket.gethostname} at #{Time.now.to_i}</CallerReference>
 </InvalidationBatch>
 XML
   end
@@ -139,7 +139,7 @@ XML
     digest = HMAC::SHA1.new(@aws_secret)
     digest << date
     signature = Base64.encode64(digest.digest)
-    {'Date' =>  date, 'Authorization' => "AWS #{@aws_key}:#{signature}"}
+    {'Date' => date, 'Authorization' => "AWS #{@aws_key}:#{signature}"}
   end
 
   class TooManyInvalidationsInProgress < StandardError ; end
